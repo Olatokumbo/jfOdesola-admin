37ee561b3968243b28bf97a5937e534c73799221
David Odesola <faithodesola@gmail.com>
2021-01-28 10:42:35 +0100

    Setting up and Designing the Signin Page

---
+++ src/pages/Signin/Signin.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { TextField, Button, Typography } from "@material-ui/core";
+import style from "./Signin.module.css";
+const Signin = () => {
+  return (
+    <div className={style.main}>
+      <div className={style.signinContainer}>
+        <div className={style.left}></div>
+        <div className={style.right}>
+          <div className={style.brand}>
+            <Typography variant="h6">JF Odesola (Admin)</Typography>
+          </div>
+          <form className={style.form}>
+            <TextField
+              name="email"
+              type="email"
+              className={style.input}
+              variant="outlined"
+              label="Email"
+              size="small"
+              required
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+            <TextField
+              name="password"
+              type="password"
+              className={style.input}
+              variant="outlined"
+              label="Password"
+              size="small"
+              required
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+            <Button
+              type="submit"
+              className={style.signin}
+              variant="contained"
+              size="small"
+              color="primary"
+            >
+              Sign In
+            </Button>
+            <Typography color="error" variant="body2">
+              {/* {error} */}
+            </Typography>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Signin;
