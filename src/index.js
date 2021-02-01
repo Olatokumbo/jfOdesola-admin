5d7383d682c76ecd63c463a04e03132954aab32a
David Odesola <faithodesola@gmail.com>
2021-02-01 10:34:19 +0100

    Setting up Redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import authReducer from "./store/reducers/auth";
 import App from "./App";
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
