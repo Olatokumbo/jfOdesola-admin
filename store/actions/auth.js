0baa40b34e15d18a0a8bee3e0c1371d34f4ff183
David Odesola <faithodesola@gmail.com>
2021-02-01 10:27:06 +0100

    Setting up the Auth Reducer and actions

---
+++ store/actions/auth.js
@@ -0,0 +1,6 @@
+// Authenication
+export const SIGNIN_SUCCESS = "SIGNIN_SUCCESS";
+export const SIGNIN_FAILED = "SIGNIN_FAILED";
+export const SIGNOUT_SUCCESS = "SIGNOUT_SUCCESS";
+export const SIGNOUT_FAILED = "SIGNOUT_FAILED";
+
---
+++ store/reducers/auth.js
@@ -0,0 +1,37 @@
+import * as actionCreator from "../actions/auth";
+const initialState = {
+    auth: false,
+    message: null
+}
+
+const authReducer = (state = initialState, action) =>{
+    switch (action.type) {
+        case actionCreator.SIGNIN_SUCCESS:
+            return{
+                ...state,
+                auth: true,
+                message:null
+            }
+        case actionCreator.SIGNIN_FAILED:
+            return{
+                ...state,
+                auth: false,
+                message: action.message
+            }
+        case actionCreator.SIGNOUT_SUCCESS:
+            return{
+                ...state,
+                auth: false,
+                message: null
+            }
+        case actionCreator.SIGNOUT_FAILED:
+            return{
+                ...state,
+                message: action.message
+            }
+        default:
+            return state;
+    }
+}
+
+export default authReducer;
